52ec2c1ad5223502852d088193cd81f51f6364ff
Akash <hafejmohammadakash@gmail.com>
2025-06-14 11:30:02 +0600

    navbar done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,75 +4,63 @@ import { NavLink } from "react-router-dom";
 const Navbar = () => {
   const links = (
     <>
-      <li>
-        <NavLink to="/">Home</NavLink>
-      </li>
-      <li>
-        <NavLink to="/allReviews">All Reviews</NavLink>
-      </li>
-      <li>
-        <NavLink to="/addReviews">Add Reviews</NavLink>
-      </li>
-      <li>
-        <NavLink to="/myReviews">My Reviews</NavLink>
-      </li>
-      <li>
-        <NavLink to="/gameWatchList">Game WatchList</NavLink>
-      </li>
+      <li><NavLink to="/">Home</NavLink></li>
+      <li><NavLink to="/allReviews">All Reviews</NavLink></li>
+      <li><NavLink to="/addReviews">Add Reviews</NavLink></li>
+      <li><NavLink to="/myReviews">My Reviews</NavLink></li>
+      <li><NavLink to="/gameWatchList">Game WatchList</NavLink></li>
     </>
   );
 
   return (
-    <div className="navbar bg-base-100 w-11/12 mx-auto">
-      <div className="navbar-start">
-        <div className="dropdown">
-          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M4 6h16M4 12h8m-8 6h16"
-              />
-            </svg>
-          </div>
-          <ul
-            tabIndex={0}
-            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
-          >
-            {links}
-          </ul>
-        </div>
-        <NavLink to="/" className="btn btn-ghost text-xl">
-          🎮 Chill Gamer
-        </NavLink>
-      </div>
-      <div className="navbar-center hidden lg:flex">
-        <ul className="menu menu-horizontal px-1">{links}</ul>
-      </div>
-
-      <div className="navbar-end">
-        <div className="flex items-center gap-3">
-          {/* darck mood */}
-          <div>
-            <label className="flex cursor-pointer gap-2">
+    <div className="bg-gradient-to-r from-cyan-700 to-blue-500">
+      <div className="navbar w-11/12 mx-auto text-white">
+        {/* Navbar Start */}
+        <div className="navbar-start">
+          {/* Dropdown for small devices */}
+          <div className="dropdown">
+            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
-                width="20"
-                height="20"
-                viewBox="0 0 24 24"
+                className="h-5 w-5"
                 fill="none"
+                viewBox="0 0 24 24"
                 stroke="currentColor"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
               >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />
+              </svg>
+            </div>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box text-black w-52"
+            >
+              {links}
+            </ul>
+          </div>
+
+          {/* Branding */}
+          <NavLink to="/" className=" normal-case text-xl">
+            🎮 <span className="ml-2 font-bold text-left">Gaming<br />World!</span>
+          </NavLink>
+        </div>
+
+        {/* Navbar Center */}
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal px-1">{links}</ul>
+        </div>
+
+        {/* Navbar End */}
+        <div className="navbar-end">
+          <div className="flex items-center gap-3">
+            {/* Dark mode toggle */}
+            <label className="flex cursor-pointer gap-2 items-center">
+              <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none"
+                   stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
                 <circle cx="12" cy="12" r="5" />
                 <path d="M12 1v2M12 21v2M4.2 4.2l1.4 1.4M18.4 18.4l1.4 1.4M1 12h2M21 12h2M4.2 19.8l1.4-1.4M18.4 5.6l1.4-1.4" />
               </svg>
@@ -81,30 +69,18 @@ const Navbar = () => {
                 value="synthwave"
                 className="toggle theme-controller"
               />
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="20"
-                height="20"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              >
+              <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"
+                   fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round"
+                   strokeLinejoin="round">
                 <path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path>
               </svg>
             </label>
-          </div>
 
-          {/* user image icon */}
-          <div className="border border-1 w-10 h-10 rounded-full border-white">
+            {/* User image icon */}
+            <div className="w-8 h-8 rounded-full border border-white bg-gray-200"></div>
 
-          </div>
-
-            {/* login user button */}
-          <div>
-            <button className="btn  border border-1 border-white rounded-full px-10">
+            {/* Login Button */}
+            <button className="btn border border-white bg-gradient-to-r from-cyan-500 to-blue-500 rounded-full px-6 text-white hover:opacity-90">
               Login
             </button>
           </div>
