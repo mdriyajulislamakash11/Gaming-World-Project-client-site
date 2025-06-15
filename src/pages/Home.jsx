6bf1b79d247adae1872f62fba7ffe809d1e27551
Akash <hafejmohammadakash@gmail.com>
2025-06-15 10:01:45 +0600

    optional chaining

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -25,7 +25,7 @@ const Home = () => {
         {/* dynamic card */}
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 my-16">
             {
-              loadedData.slice(0, 8).map((data) => <GameCard key={data._id} data={data} />)
+              loadedData.slice(0, 8)?.map((data) => <GameCard key={data._id} data={data} />)
             }
         </div>
         
